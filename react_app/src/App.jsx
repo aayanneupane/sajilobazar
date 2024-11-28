78a7c35204b36addc2ef16c2313bcc8ccecabd01
Niraj <maharjanniraj87@gmail.com>
2024-11-28 22:14:55 +0545

    last check

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
+import Nav from "./components/Nav"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+    <Nav />
       <h1>Software Engineering</h1>
-        
     </div>
   )
 }
